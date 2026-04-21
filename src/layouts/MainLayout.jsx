9b2a07bef359803a96196ce38644fdb07ba1250e
kanchan kahar <kaharkanchan884@gmail.com>
2026-04-22 01:14:32 +0530

    fix bugs

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/common/Navbar";
+import Footer from "../components/common/Footer";
+
+export default function MainLayout({ children }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
