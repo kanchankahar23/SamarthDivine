1876c25609bd01789ac053d400e5cee807f2764b
kanchan kahar <kaharkanchan884@gmail.com>
2026-04-22 01:06:18 +0530

    update

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -6,6 +6,7 @@ export default function MainLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
